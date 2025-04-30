96777cd650aee00a9f056d9fcbe5d412de3d0f62
Amen <amenaf244@gmail.com>
2025-04-30 11:33:29 +0300

    changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ function Index() {
       <meta name='auther' content='Amen AF' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     </head>
-      <img className={style.logo} src="public/images/logo.jpg" alt="logo" />
+      <img className={style.logo} src="travel/public/images/logo.jpg" alt="logo" />
       <Navegator />
       <div className={style.sliderContainer}>
         <div className={style.slider}>
